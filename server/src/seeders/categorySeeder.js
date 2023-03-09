699598e60bc486958663411c16343dca0c2d96a5
NghiemTuan <tuannghiemtien97@gmail.com>
2023-03-09 07:31:37 +0700

    feat: handle seeder categories

--- server/src/seeders/categorySeeder.js
+++ server/src/seeders/categorySeeder.js
@@ -1,17 +1,57 @@
 import {Category} from '../entities/Category';
 import {dataConnection} from '../config/database';
+import {SubCategory} from "../entities/SubCategory";
 
-const data = [
+const categories = [
   {
-    name: 'category',
+    name: "Món chính",
+    sub: [
+      'Cơm',
+      'Gà',
+      'Bò',
+      'Cá',
+      'Món chay',
+    ],
   },
-];
+  {
+    name: "Món Phụ",
+    sub: [
+      'Ăn vặt',
+      'Ăn sáng',
+      'Khai vị',
+    ],
+  },
+  {
+    name: "Nhanh - dễ",
+    sub: [
+      'Bún',
+      'Phở',
+    ],
+  },
+  {
+    name: "Lẩu",
+    sub: [
+      'Lẩu gà',
+      'Lẩu vịt',
+      'Lẩu đuôi bò',
+    ],
+  },
+]
 
 export const categorySeeder = async () => {
   const categoryRepository = dataConnection.getRepository(Category);
-  for (let item of data) {
+  const subCategoryRepository = dataConnection.getRepository(SubCategory);
+
+  for (let item of categories) {
     const category = new Category();
     category.name = item.name;
-    await categoryRepository.save(category);
+    const categoryDB = await categoryRepository.save(category);
+
+    for (let sub of item.sub) {
+      const subCategory = new SubCategory();
+      subCategory.name = sub;
+      subCategory.category_id = categoryDB.id;
+      await subCategoryRepository.save(subCategory);
+    }
   }
 };
